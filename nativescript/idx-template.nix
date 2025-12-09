970282e818eb30bc319c4978ed9b014ca8330386
Harsha-Adiboina <AdiboinaHarshaKiran.Yadav@cognizant.com>
2025-12-10 00:55:25 +0530

    Update idx-template.nix

--- nativescript/idx-template.nix
+++ nativescript/idx-template.nix
@@ -29,7 +29,7 @@
     shopt -s dotglob; cp -r ${./dev}/* "$out"
     if [ "${template}" = "svelte" ]; then
        npm config set legacy-peer-deps true
-       npx ns create example --template @nativescript/template-blank-svelte --path "$out"
+       npx ns create "$out" --template @nativescript/template-blank-svelte
     else
        npm install nativescript
        ./node_modules/nativescript/bin/ns create example --${template} ${if ts then "--ts" else ""} --path "$out"
