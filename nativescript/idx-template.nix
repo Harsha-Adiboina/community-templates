59d312470242c370c3fdc15804fdaae8255cec5a
Harsha-Adiboina <AdiboinaHarshaKiran.Yadav@cognizant.com>
2025-12-10 12:38:34 +0530

    Update idx-template.nix

--- nativescript/idx-template.nix
+++ nativescript/idx-template.nix
@@ -16,7 +16,7 @@
 
 { pkgs, template ? "js", ts ? false, ... }: {
   packages = [
-    pkgs.nodejs_20
+    pkgs.nodejs_24
     pkgs.python3
     pkgs.python311Packages.pip
     pkgs.python311Packages.fastapi
@@ -30,8 +30,8 @@
 
     if [ "${template}" = "svelte" ]; then
        npm config set legacy-peer-deps true
-       npm install --save-dev nativescript@8.6.1
-       npx ns create example --template @nativescript/template-blank-svelte --path "$out"
+       npm install --save-dev nativescript
+       npx ns create example --svelte --path "$out"
     else
        npm install nativescript
        ./node_modules/nativescript/bin/ns create example --${template} ${if ts then "--ts" else ""} --path "$out"
