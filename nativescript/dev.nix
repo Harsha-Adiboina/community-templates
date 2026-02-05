57aafa99ecf19749513eb087d3147e46ab0f8e46
adiboinaharshakiran.yadav <adiboinaharshakiran.yadav@cognizant.com>
2026-02-05 10:41:45 +0000

    Adding FB analytics

--- nativescript/dev.nix
+++ nativescript/dev.nix
@@ -1,13 +1,13 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs_20
     pkgs.jdk17
-  ];
+  ] ++ (lib.optional (lib.getParam "firebase" false) pkgs.google-cloud-sdk);
   # Sets environment variables in the workspace
   env = { };
   idx = {
@@ -22,6 +22,9 @@
       onCreate = {
         install = ''
           npm ci --prefer-offline --no-audit --no-progress --timing
+          if [ "${lib.getParam "firebase" false}" = "true" ]; then
+            npm install --save @nativescript/firebase-analytics
+          fi
           yes | npx ns preview'';
         # Open editors for the following files by default, if they exist:
         default.openFiles = [ "app/app.js" ];
